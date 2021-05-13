915f7be79a6aeb09cc6150755391db595396ce1b
wanby <wan7631>
2021-05-13 16:07:41 +0800

    build

---
+++ dist/hooks/useSpring.d.ts
@@ -0,0 +1,17 @@
+export declare function useSpring(): [import("react-spring").AnimatedValue<import("react-spring").ForwardedProps<{
+    y: unknown;
+    ready: unknown;
+    maxHeight: import("csstype").Property.MaxHeight<string | number>;
+    minSnap: unknown;
+    maxSnap: unknown;
+    topOffset: unknown;
+}>>, import("react-spring").SetUpdateFn<{
+    y: unknown;
+    ready: unknown;
+    maxHeight: import("csstype").Property.MaxHeight<string | number>;
+    minSnap: unknown;
+    maxSnap: unknown;
+    topOffset: unknown;
+}>];
+export declare type Spring = ReturnType<typeof useSpring>[0];
+export declare type SpringSet = ReturnType<typeof useSpring>[1];
