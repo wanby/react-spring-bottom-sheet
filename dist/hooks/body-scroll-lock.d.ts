f194b094b1501e8647b477a7164ad9bd9ca5c52f
wanby <wanbing.yang@gmail.com>
2021-05-19 13:13:45 +0800

    build dist

---
+++ dist/hooks/body-scroll-lock.d.ts
@@ -0,0 +1,7 @@
+export interface BodyScrollOptions {
+    reserveScrollBarGap?: boolean;
+    allowTouchMove?: (el: any) => boolean;
+}
+export declare const disableBodyScroll: (targetElement: any, options?: BodyScrollOptions) => void;
+export declare const clearAllBodyScrollLocks: () => void;
+export declare const enableBodyScroll: (targetElement: any) => void;
